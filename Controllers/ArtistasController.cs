84dc4d801a652716a62d3b661493b34deded141d
laboratorio <laboratorio@UPOAULA10613>
2023-11-01 08:47:30 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ArtistasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WF.clase.Data;
+using WF.clase.Models;
+
+namespace WF.clase.Controllers
+{
+    public class ArtistasController : Controller
+    {
+        private WFclaseContext db = new WFclaseContext();
+
+        // GET: Artistas
+        public ActionResult Index()
+        {
+            return View(db.Artistas.ToList());
+        }
+
+        // GET: Artistas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artista artista = db.Artistas.Find(id);
+            if (artista == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artista);
+        }
+
+        // GET: Artistas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artistas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ArtistaID,NombrePilaArtista,NombreArtistico,FechaNacimiento")] Artista artista)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Artistas.Add(artista);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(artista);
+        }
+
+        // GET: Artistas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artista artista = db.Artistas.Find(id);
+            if (artista == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artista);
+        }
+
+        // POST: Artistas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ArtistaID,NombrePilaArtista,NombreArtistico,FechaNacimiento")] Artista artista)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artista).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artista);
+        }
+
+        // GET: Artistas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artista artista = db.Artistas.Find(id);
+            if (artista == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artista);
+        }
+
+        // POST: Artistas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Artista artista = db.Artistas.Find(id);
+            db.Artistas.Remove(artista);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
